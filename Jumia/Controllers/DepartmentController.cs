a6a61087cd726a1a13e5209aa6d4c5204d31704e
ibrahim4626 <ibrahimsaad2310@gmail.com>
2024-10-03 14:43:59 +0300

    create GetAllDepartments

--- Jumia/Controllers/DepartmentController.cs
+++ Jumia/Controllers/DepartmentController.cs
@@ -1,14 +1,24 @@
+using Jumia.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Jumia.Controllers
 {
     public class DepartmentController : Controller
     {
+        AppDBContext context = new AppDBContext();
+        public DepartmentController(AppDBContext _context) 
+        {
+            context = _context;
+        }
         // Action for get all departments 
         public IActionResult GetAllDepartments()
         {
-            ViewData["Title"] = "Men's Fashion";
-            return View("Index");  // Ensure this returns the correct view (Views/Department/Men.cshtml)
+            List<Department> departments = new List<Department>();
+            departments = context.Departments.ToList();
+            ViewBag.Departments = departments;
+
+            //ViewData["Title"] = "Men's Fashion";
+            return View(departments);  // Ensure this returns the correct view (Views/Department/Men.cshtml)
         }
 
     }
