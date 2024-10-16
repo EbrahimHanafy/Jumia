4c20f804874c2856362aab31dd71e154a8aa08e1
ibrahim4626 <ibrahimsaad2310@gmail.com>
2024-10-16 03:39:19 +0300

    Shopping cartRepository

--- Jumia/Repositories/Implementation/ShoppingCartRepository.cs
+++ Jumia/Repositories/Implementation/ShoppingCartRepository.cs
@@ -13,5 +13,30 @@ namespace Jumia.Repositories.Implementation
 		{
 			_shoppingCarts = context.Set<ShoppingCart>();
 		}
-	}
+
+        public Task AddProductToCart(int productId, int quantity, int ProductColorSizeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DecreaseItemQty(int productId, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShoppingCart> GetCartProducts(int userCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task IncreaseItemQty(int productId, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveItem(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
